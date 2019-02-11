2981b16373bc9c0e2134f29bc46d96cd8260839b
leonidk <lkeselman@gmail.com>
2019-02-11 10:23:41 -0500

    this should be okay

--- registration_test.py
+++ registration_test.py
@@ -13,14 +13,14 @@ import transforms3d
 from pycpd import rigid_registration
 import time
 
-SAMPLE_NUM = 10
+SAMPLE_NUM = 100
 method = None#'CG'
-K = 100
-SAMPLE_PTS = 453
-ICP_ITERS = 10000 #150
+K = 50
+SAMPLE_PTS = 948
+ICP_ITERS = 50000 #150
 ICP_THRESH = 1e-9
 CPD_THRESH = 1e-9
-CPD_ITERS = 2000 #500
+CPD_ITERS = 100 #500
 mesh0 = pymesh.load_mesh("bunny/bun_zipper_res4.ply")
 mesh_pts = pymesh.load_mesh("bunny/bun_zipper_res4_sds.ply")
 
@@ -34,26 +34,28 @@ def get_centroids(mesh):
     return centroids, areas
 
 com,a = get_centroids(mesh0)
+print(com.shape)
 face_vert = mesh0.vertices[mesh0.faces.reshape(-1),:].reshape((mesh0.faces.shape[0],3,-1))
 
 #gm3 = GaussianMixture(100,init_params='kmeans'); gm3.set_triangles(face_vert); gm3.fit(com); gm3.set_triangles(None)
 #usually tol=1e-4,max_iter=100
 t1 = time.time()
-gm_std = GaussianMixture(K,init_params='kmeans',tol=1e-9,max_iter=20); gm_std.fit(com)
+gm_std_km = GaussianMixture(K,init_params='kmeans',tol=1e-5,max_iter=100); gm_std_km.fit(com)
 print((time.time()-t1)*1000)
 t1 = time.time()
-gm_mesh = GaussianMixture(K,init_params='random',tol=1e-9,max_iter=20); gm_mesh.set_triangles(face_vert); gm_mesh.fit(com); gm_mesh.set_triangles(None)
+gm_std = GaussianMixture(K,init_params='random',tol=1e-5,max_iter=100); gm_std.fit(com)
 print((time.time()-t1)*1000)
-indices2 = np.random.randint(0,mesh_pts.vertices.shape[0],SAMPLE_PTS)
-samples_for_icp = mesh_pts.vertices[indices2]
-full_points = samples_for_icp#mesh_pts.vertices
-indices = np.random.randint(0,full_points.shape[0],SAMPLE_PTS)
-samples= full_points
+t1 = time.time()
+gm_mesh = GaussianMixture(K,init_params='random',tol=1e-5,max_iter=100); gm_mesh.set_triangles(face_vert); gm_mesh.fit(com); gm_mesh.set_triangles(None)
+print((time.time()-t1)*1000)
+
 
 data_log_mesh = []
 data_log_verts = []
+data_log_vertsk = []
 data_log_icp = []
 data_log_cpd = []
+data_log_oracle = []
 opt_times = []
 opt_times_pts = []
 icp_times = []
@@ -61,6 +63,15 @@ cpd_times = []
 
 prev_time = time.time()
 for n in range(SAMPLE_NUM):
+
+    indices2 = np.random.randint(0,mesh_pts.vertices.shape[0],SAMPLE_PTS)
+    samples_for_icp = np.copy(mesh_pts.vertices[indices2])
+    full_points = samples_for_icp#mesh_pts.vertices
+    indices = np.random.randint(0,mesh_pts.vertices.shape[0],SAMPLE_PTS)
+    samples = np.copy(mesh_pts.vertices[indices])
+    samples_mean = samples.mean(0)
+    centered_points = samples - samples_mean
+
     print(n,round(time.time()-prev_time,1),'seconds')
     prev_time = time.time()
     if False: # random transformations
@@ -75,11 +86,20 @@ for n in range(SAMPLE_NUM):
         M = transforms3d.euler.euler2mat(angles[0],angles[1],angles[2])
 
     true_q = transforms3d.quaternions.mat2quat(M)
-    samples_mean = samples.mean(0)
-    centered_points = samples - samples_mean
+
     source = centered_points @ M + samples_mean+ t
     sourcemean = source.mean(0)
     source_centered = source - sourcemean
+
+    H = (source-source.mean(0)).T @ (samples-samples.mean(0))
+    u,s,vt = np.linalg.svd(H)
+    R_reg = vt.T @ np.diag([1,1,np.linalg.det(vt.T @ u.T)]) @ u.T
+    t_reg = source.mean(0)-samples.mean(0)
+    oracle_q = transforms3d.quaternions.mat2quat(R_reg)
+    data_log_oracle.append( [oracle_q.dot(true_q),np.linalg.norm(t_reg-t)] )
+
+
+
     def loss_verts(x):
         qs = x[:4]
         ts = x[4:]
@@ -111,6 +131,22 @@ for n in range(SAMPLE_NUM):
     rq = rq/np.linalg.norm(rq)
     rt = res.x[4:]
     data_log_mesh.append( [rq.dot(true_q),np.linalg.norm(rt-t)] )
+    def loss_mesh_k(x):
+        qs = x[:4]
+        ts = x[4:]
+        qs = qs/np.linalg.norm(qs)
+        Ms = transforms3d.quaternions.quat2mat(qs)
+        tpts =  (source_centered) @ Ms.T + sourcemean - ts
+        return -gm_std_km.score(tpts)
+    start_opt = time.time()
+    res = opt.minimize(loss_mesh_k,np.array([1,0,0,0,0,0,0]),method=method)
+    end_opt = time.time()
+    opt_times.append(end_opt-start_opt)
+    rq = res.x[:4]
+    rq = rq/np.linalg.norm(rq)
+    rt = res.x[4:]
+    data_log_vertsk.append( [rq.dot(true_q),np.linalg.norm(rt-t)] )
+
 
     icp_t = np.zeros(3)
     R = np.identity(3)
@@ -184,8 +220,10 @@ print(np.array(opt_times_pts).mean()*1000)
 print(np.array(opt_times).mean()*1000)
 print(np.array(icp_times).mean()*1000)
 print(np.array(cpd_times).mean()*1000)
-
-np.savetxt('verts2.csv',np.array(data_log_verts),delimiter=',')
-np.savetxt('mesh2.csv',np.array(data_log_mesh),delimiter=',')
-np.savetxt('icp2.csv',np.array(data_log_icp),delimiter=',')
-np.savetxt('cpd2.csv',np.array(data_log_cpd),delimiter=',')
+if len(data_log_verts) > 0 :
+    np.savetxt('verts2.csv',np.array(data_log_verts),delimiter=',')
+    np.savetxt('vertsk2.csv',np.array(data_log_vertsk),delimiter=',')
+    np.savetxt('mesh2.csv',np.array(data_log_mesh),delimiter=',')
+    np.savetxt('icp2.csv',np.array(data_log_icp),delimiter=',')
+    np.savetxt('cpd2.csv',np.array(data_log_cpd),delimiter=',')
+    np.savetxt('oracle2.csv',np.array(data_log_oracle),delimiter=',')
