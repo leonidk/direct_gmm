5603ae8bbb9447f3ae6cc707719238e717c6a2a7
leonidk <lkeselman@gmail.com>
2019-02-19 03:18:46 -0500

    submit1

--- gmm_fit.py
+++ gmm_fit.py
@@ -102,14 +102,14 @@ data_covar2 = get_tri_covar(fv2)
 #res  = compute_gmm(com,100,a)
 #res2 = compute_gmm(verts,100)
 #raise
-with open('bunny_fit_monday_subsamples_50.log','w') as fout:
+with open('bunny_fit_monday_subsamples_25.log','w') as fout:
     for km in [6,12,25,50,100,200,400]:
         for init in ['random','kmeans']:
             for exp_n in range(10):
-                gm0 = GaussianMixture(km,init_params=init,max_iter=50,tol=1e-12); gm0.set_covars(data_covar1); gm0.set_areas(aa); gm0.fit(coma); gm0.set_covars(None);  gm0.set_areas(None)
-                gm1 = GaussianMixture(km,init_params=init,max_iter=50,tol=1e-12); gm1.set_covars(data_covar2); gm1.set_areas(a); gm1.fit(com); gm1.set_covars(None);  gm1.set_areas(None)
-                gm2 = GaussianMixture(km,init_params=init,max_iter=50,tol=1e-12); gm2.fit(mesh3.vertices)
-                gm3 = GaussianMixture(km,init_params=init,max_iter=50,tol=1e-12); gm3.fit(mesh2.vertices)
+                gm0 = GaussianMixture(km,init_params=init,max_iter=25,tol=1e-12); gm0.set_covars(data_covar1); gm0.set_areas(aa); gm0.fit(coma); gm0.set_covars(None);  gm0.set_areas(None)
+                gm1 = GaussianMixture(km,init_params=init,max_iter=25,tol=1e-12); gm1.set_covars(data_covar2); gm1.set_areas(a); gm1.fit(com); gm1.set_covars(None);  gm1.set_areas(None)
+                gm2 = GaussianMixture(km,init_params=init,max_iter=25,tol=1e-12); gm2.fit(mesh3.vertices)
+                gm3 = GaussianMixture(km,init_params=init,max_iter=25,tol=1e-12); gm3.fit(mesh2.vertices)
 
                 #gm3 = GaussianMixture(100); gm3.fit(mesh4.vertices)
                 #print(coma.shape[0],com.shape[0],mesh2.vertices.shape[0],mesh3.vertices.shape[0])
